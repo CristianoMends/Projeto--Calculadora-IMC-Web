fcb01f3ee9e640f9f7910d932cfe6e16010be866
CristianoMends <mendescristiano012@gmail.com>
2023-03-26 14:17:34 -0300

    atualizacao da interface

--- Js/app.js
+++ Js/app.js
@@ -1,20 +1,9 @@
-function calc(){
-    var altura = parseFloat(document.getElementById("altura").value);
-    var peso = parseFloat(document.getElementById("peso").value);
-    var imc = peso/(altura*altura);
-    var fimc = imc.toFixed(2);
+function calculate(){
+    let height = parseFloat(document.getElementById("height").value);
+    let weight = parseFloat(document.getElementById("weight").value);
+    let imc = weight/(height*height).toFixed(2);
     
-    if(altura > 0 && peso > 0){
-    window.alert("O seu Indice de massa corporal é "+fimc);
+    window.alert(imc);
     
-    if(fimc < 25 && fimc > 18){
-        window.alert("Seu imc esta regular!");
-    }
-    if(fimc < 18){
-        window.alert("Você está abaixo do peso!");
-    }
-}else{
-    window.alert("Digite um valor valido!")
-}
 }
 
